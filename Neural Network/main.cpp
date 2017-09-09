87f4084c1ec35369364c9b3f17c3094ac86eda69
Dylan Seto <dylan.seto@japache.ca>
2017-09-09 18:48:53 -0400

    adds SFML library

--- Neural Network/main.cpp
+++ Neural Network/main.cpp
@@ -3,16 +3,20 @@
 #include "Agent.h"
 #include <iostream>
 #include <time.h>
+#include "World.h"
 using namespace std;
 
 int main()
 {
-	srand(time(NULL));
-	/*for (int i = 0; i != 5; i++)
-		cout << MathHelper::RandomNumberClamped() << endl;*/
-	Agent* test = new Agent();
+	//srand(time(NULL));
+	///*for (int i = 0; i != 5; i++)
+	//	cout << MathHelper::RandomNumberClamped() << endl;*/
+	//Agent* test = new Agent();
 
-	test->tick();
+	//test->tick();*/
+
+	World* world = new World();
+	world->init();
 
 	system("pause");
 }
